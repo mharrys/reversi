0e0caabdcb64ae4a7bb9bfb7d16434c8819bcaf9
Mattias Harrysson <mathar@kth.se>
2015-12-26 17:32:21 +0100

    add square

---
+++ src/Square.hs
@@ -0,0 +1,12 @@
+module Square
+    ( Square(..)
+    ) where
+
+import Piece (Piece(..))
+
+-- | Describes a square of a board that may have a placed piece in it.
+data Square = Square (Maybe Piece) deriving (Eq)
+
+instance Show Square where
+    show (Square Nothing)  = " "
+    show (Square (Just x)) = show x
---
+++ test/SquareSpec.hs
@@ -0,0 +1,16 @@
+module SquareSpec where
+
+import Test.Hspec
+
+import Piece
+import Square
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    it "should be represented as a string" $ do
+        show (Square Nothing) `shouldBe` " "
+        show (Square (Just Black)) `shouldBe` show Black
+        show (Square (Just White)) `shouldBe` show White
