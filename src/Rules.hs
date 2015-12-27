97d590a647ab7fbea8712fcbaa27c9e0ec2b1498
Mattias Harrysson <mathar@kth.se>
2015-12-27 18:46:46 +0100

    add rule for checking if piece has any possible move

--- src/Rules.hs
+++ src/Rules.hs
@@ -1,9 +1,9 @@
 module Rules where
 
-import Board (Board, Point, Cell, cell, inside)
+import Board (Board, Point, Cell, cell, inside, unoccupiedCells)
 import Move (Move(..), Direction, directions, pointsInDirection)
 import Piece (Piece)
-import Square (occupied, occupiedBy)
+import Square (Square(..), occupied, occupiedBy)
 
 -- | Return nodes that will be flipped for a move at given board.
 nodesToFlip :: Move -> Board -> [Cell]
@@ -34,3 +34,10 @@ nodesToFlip (Move piece p) b = concatMap (\x -> captures x []) ps
 -- | Validate if move is allowed.
 isMoveValid :: Move -> Board -> Bool
 isMoveValid m b = null (nodesToFlip m b)
+
+-- | Determine if piece has any valid move.
+hasValidMove :: Piece -> Board -> Bool
+hasValidMove piece b = any canMove (unoccupiedCells b)
+  where
+    canMove :: Cell -> Bool
+    canMove (p, _) = isMoveValid (Move piece p) b
--- test/RulesSpec.hs
+++ test/RulesSpec.hs
@@ -13,15 +13,20 @@ main = hspec spec
 
 spec :: Spec
 spec =
-    it "should return correct captures on initial standard board" $ do
-        nodesToFlip (Move Black (3, 2)) standardBoard `shouldBe` [((3, 3), Occupied White)]
-        nodesToFlip (Move Black (2, 3)) standardBoard `shouldBe` [((3, 3), Occupied White)]
-        nodesToFlip (Move Black (5, 4)) standardBoard `shouldBe` [((4, 4), Occupied White)]
-        nodesToFlip (Move Black (4, 5)) standardBoard `shouldBe` [((4, 4), Occupied White)]
-        nodesToFlip (Move White (2, 4)) standardBoard `shouldBe` [((3, 4), Occupied Black)]
-        nodesToFlip (Move White (3, 5)) standardBoard `shouldBe` [((3, 4), Occupied Black)]
-        nodesToFlip (Move White (4, 2)) standardBoard `shouldBe` [((4, 3), Occupied Black)]
-        nodesToFlip (Move White (5, 3)) standardBoard `shouldBe` [((4, 3), Occupied Black)]
-        nodesToFlip (Move White (0, 0)) standardBoard `shouldBe` []
-        nodesToFlip (Move White (3, 2)) standardBoard `shouldBe` []
-        nodesToFlip (Move Black (2, 4)) standardBoard `shouldBe` []
+    context "standard board" $
+        context "initial state" $ do
+            it "should return correct captures on initial state" $ do
+                nodesToFlip (Move Black (3, 2)) standardBoard `shouldBe` [((3, 3), Occupied White)]
+                nodesToFlip (Move Black (2, 3)) standardBoard `shouldBe` [((3, 3), Occupied White)]
+                nodesToFlip (Move Black (5, 4)) standardBoard `shouldBe` [((4, 4), Occupied White)]
+                nodesToFlip (Move Black (4, 5)) standardBoard `shouldBe` [((4, 4), Occupied White)]
+                nodesToFlip (Move White (2, 4)) standardBoard `shouldBe` [((3, 4), Occupied Black)]
+                nodesToFlip (Move White (3, 5)) standardBoard `shouldBe` [((3, 4), Occupied Black)]
+                nodesToFlip (Move White (4, 2)) standardBoard `shouldBe` [((4, 3), Occupied Black)]
+                nodesToFlip (Move White (5, 3)) standardBoard `shouldBe` [((4, 3), Occupied Black)]
+                nodesToFlip (Move White (0, 0)) standardBoard `shouldBe` []
+                nodesToFlip (Move White (3, 2)) standardBoard `shouldBe` []
+                nodesToFlip (Move Black (2, 4)) standardBoard `shouldBe` []
+            it "should have valid moves for both players" $ do
+                hasValidMove White standardBoard `shouldBe` True
+                hasValidMove Black standardBoard `shouldBe` True
