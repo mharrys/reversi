327bc87f39bc83cff3af76d8aa5f2a568b92c3fe
Mattias Harrysson <mathar@kth.se>
2015-12-27 23:09:00 +0100

    coord is now created from a letter and a digit

--- src/Coord.hs
+++ src/Coord.hs
@@ -3,18 +3,28 @@ module Coord
     , readMaybeCoord
     ) where
 
+import Data.Char (ord, isDigit, isAsciiLower)
 import Text.Read (readMaybe)
 
 data Coord = Coord Int Int deriving (Eq)
 
 instance Show Coord where
-    show (Coord x y) = show x ++ " " ++ show y
+    show (Coord row col) = toChar col : (show . succ) row
+      where
+        toChar x = ['a'..'z'] !! x
 
 instance Read Coord where
-    readsPrec d r = do
-        (x, r')  <- readsPrec d r
-        (y, r'') <- readsPrec d r'
-        return (Coord x y, r'')
+    readsPrec _ []             = []
+    readsPrec _ input
+        | validRow && validCol = [(Coord row col, input'')]
+        | otherwise            = []
+      where
+        validRow     = (not . null) r && row >= 0 && row <= 25
+        validCol     = isAsciiLower c
+        row          = (read r :: Int) - 1
+        col          = ord c - 97
+        (r, input'') = span isDigit input'
+        (c:input')   = input
 
 readMaybeCoord :: String -> Maybe Coord
 readMaybeCoord = readMaybe
--- test/CoordSpec.hs
+++ test/CoordSpec.hs
@@ -13,20 +13,21 @@ main = hspec spec
 spec :: Spec
 spec = do
     it "should be represented as a string" $ do
-        property $ \x y ->
-            show (Coord (x :: Int) (y :: Int))
-            ==
-            show x ++ " " ++ show y
+        show (Coord 0 0) `shouldBe` "a1"
+        show (Coord 3 4) `shouldBe` "e4"
+        show (Coord 7 7) `shouldBe` "h8"
+        show (Coord 13 20) `shouldBe` "u14"
     it "should accept valid input" $ do
-        property $ \x y ->
-            (read (show x ++ " " ++ show y) :: Coord)
-            ==
-            Coord (x :: Int) (y :: Int)
+        readMaybeCoord "a1" `shouldBe` Just (Coord 0 0)
+        readMaybeCoord "z1" `shouldBe` Just (Coord 0 25)
+        readMaybeCoord "a26" `shouldBe` Just (Coord 25 0)
+        readMaybeCoord "e4" `shouldBe` Just (Coord 3 4)
     it "should not accept invalid input" $ do
-        readMaybeCoord "0.0 0" `shouldBe` Nothing
-        readMaybeCoord "1 a" `shouldBe` Nothing
-        readMaybeCoord "5 9 3" `shouldBe` Nothing
-        readMaybeCoord "8" `shouldBe` Nothing
-        readMaybeCoord "8 0." `shouldBe` Nothing
-        readMaybeCoord "x 0" `shouldBe` Nothing
-        readMaybeCoord "  0" `shouldBe` Nothing
+        readMaybeCoord "a0" `shouldBe` Nothing
+        readMaybeCoord "aa1" `shouldBe` Nothing
+        readMaybeCoord "1a" `shouldBe` Nothing
+        readMaybeCoord "g1." `shouldBe` Nothing
+        readMaybeCoord "c1.0" `shouldBe` Nothing
+        readMaybeCoord "11" `shouldBe` Nothing
+        readMaybeCoord "gl" `shouldBe` Nothing
+        readMaybeCoord "" `shouldBe` Nothing
